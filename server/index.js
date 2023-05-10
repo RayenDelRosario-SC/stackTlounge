8042b8ce297ca246eec10467edad96c292c44b2d
RayenDelRosario-SC <rayen.delrosario.s@southlandcollege.edu.ph>
2023-05-10 16:40:41 +0800

    added new file

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+//middleware
+app.use(express.json)//req.body
+app.use(cors());
+
+//ROUTES//
+
+
+//register and login routes
+
+app.use("/auth", require("./routes/jwtAuth"));
+
+
+//dashboard routes
+app.use("/dashboard", require("./routes/dashboard"));
+
+
+app.listen(5000, () => {
+    console.log("server is running on port 5000");
+});
